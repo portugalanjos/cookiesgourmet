940a5d81d52e85636b646fe024bc74fb0a486f70
portugalanjos <portugalanjos@gmail.com>
2024-09-09 17:03:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Doce de Leite",
+        descricao: "Imagine saborear um cookie macio por dentro e crocante por fora, repleto de um irresistível recheio de doce de leite artesanal. Cada mordida é uma explosão de sabor, com a doçura caramelizada do doce de leite contrastando com a textura leve da massa. Uma verdadeira experiência para os amantes de doces caseiros!",
+        tags: "doce leite"
+    },
+    {
+        titulo: "Ninho",
+        descricao: "Deixe-se levar por uma explosão de sabor com nosso cookie de Ninho! Uma massa macia e leve envolve um irresistível recheio cremoso de leite Ninho, proporcionando uma experiência única e deliciosa. Ideal para quem ama a combinação perfeita entre o doce e o cremoso.",
+        tags: "leite ninho"
+    },
+    {
+        titulo: "Nutella",
+        descricao: "Indulja-se no paraíso com nosso cookie de Nutella! Uma massa crocante por fora e macia por dentro, recheada com a irresistível Nutella, criando uma explosão de sabor em cada mordida. Perfeito para os amantes de chocolate e avelã.",
+        tags: "creme avelã"
+    }
+];
